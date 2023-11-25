958041111df22d05f35dbc2f0e60e33e4bf6fbbd
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2023-11-25 11:21:18 +0900

    ステータスにコマンド説明

--- src/space/yurisi/Task/SendTask.php
+++ src/space/yurisi/Task/SendTask.php
@@ -21,11 +21,13 @@ class SendTask extends Task {
     $player = $this->player;
     $this->RemoveData($player);
     $this->setupData($player);
-    $this->sendData($player, "§b座標: " . $player->getPosition()->getFloorX() . "," . $player->getPosition()->getFloorY() . "," . $player->getPosition()->getFloorZ(), 1);
-    $this->sendData($player, "§bワールド: " . $player->getWorld()->getFolderName(), 2);
-    $this->sendData($player, "§c現在時刻: " . date("G時i分s秒"), 3);
-    $this->sendData($player, "§6持ってるid: " . $player->getInventory()->getItemInHand()->getId() . ":" . $player->getInventory()->getItemInHand()->getMeta(), 4);
-    $this->sendData($player, "§6オンライン人数: " . count(Server::getInstance()->getOnlinePlayers()) . "/" . Server::getInstance()->getMaxPlayers(), 5);
+    $this->sendData($player, "§a座標: " . $player->getPosition()->getFloorX() . "," . $player->getPosition()->getFloorY() . "," . $player->getPosition()->getFloorZ(), 1);
+    $this->sendData($player, "§aワールド: " . $player->getWorld()->getFolderName(), 2);
+    $this->sendData($player, "§b現在時刻: " . date("G時i分"), 3);
+    $this->sendData($player, "§c持ってるid: " . $player->getInventory()->getItemInHand()->getId() . ":" . $player->getInventory()->getItemInHand()->getMeta(), 4);
+    $this->sendData($player, "§7基本コマンド説明", 5);
+    $this->sendData($player, "§7/m 自分のワールド", 6);
+    $this->sendData($player, "§7/t {名前} 人のワールド", 7);
 
   }
 
@@ -37,7 +39,7 @@ class SendTask extends Task {
     $pk = new SetDisplayObjectivePacket();
     $pk->displaySlot = "sidebar";
     $pk->objectiveName = "sidebar";
-    $pk->displayName = "§a" . $player->getName();
+    $pk->displayName = "§6KomugiBuild";
     $pk->criteriaName = "dummy";
     $pk->sortOrder = 0;
     $player->getNetworkSession()->sendDataPacket($pk);
